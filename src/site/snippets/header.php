8ece68afdffca762a49552c0a3f28b512d0e0955
Booozz <blody.ti@googlemail.com>
2017-07-07 12:54:15 +0200

    exclude head from header snippet and logo background integration on home

--- src/site/snippets/header.php
+++ src/site/snippets/header.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title><?php echo $site->title() ?></title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="<?php echo $site->description() ?>">
-    <meta name="keywords" content="<?php echo $site->keywords() ?>">
-    <meta name="msapplication-TileColor" content="#062299">
-    <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/favicon.ico">
-    <!--build:css assets/css/main.min.css-->
-    <link rel="stylesheet" type="text/css" href="/assets/css/main.min.css">
-    <!--endbuild-->
-</head>
-<body>
-    <header class="header-main">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="logo">
-                        <a href="<?php echo $site->url() ?>"><?php echo $site->title() ?></a>
-                    </h1>
-                    <p class="phone">Rufen Sie an: &nbsp;<em>0365 - 831 02 54</em></p>
-                </div>
+<header class="header-main">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="logo">
+                    <a href="<?php echo $site->url() ?>"><?php echo $site->title() ?></a>
+                </h1>
+                <p class="phone">Rufen Sie an: &nbsp;<em>0365 - 831 02 54</em></p>
             </div>
         </div>
-    </header>
-    <?php snippet('menu') ?>
+    </div>
+</header>
+<?php snippet('menu') ?>
--- src/site/templates/home.php
+++ src/site/templates/home.php
@@ -1,15 +1,30 @@
-<?php snippet('header') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $site->title() ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="<?php echo $site->description() ?>">
+    <meta name="keywords" content="<?php echo $site->keywords() ?>">
+    <meta name="msapplication-TileColor" content="#062299">
+    <link rel="shortcut icon" sizes="16x16 24x24 32x32 48x48 64x64" href="/favicon.ico">
+    <!--build:css assets/css/main.min.css-->
+    <link rel="stylesheet" type="text/css" href="/assets/css/main.min.css">
+    <!--endbuild-->
+</head>
+<body class="home">
+    <?php snippet('header') ?>
 
-    <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="intro">
-                        <p><?php echo $page->text() ?></p>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="intro">
+                            <p><?php echo $page->text() ?></p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
+        </main>
 
-<?php snippet('footer') ?>
+    <?php snippet('footer') ?>
