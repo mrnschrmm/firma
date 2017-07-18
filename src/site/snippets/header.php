51df4471ebc6eb95fc2be4c2f702ae050f3cdb88
Booozz <blody.ti@googlemail.com>
2017-07-18 15:55:05 +0200

    change style and content of header company info

--- src/site/snippets/header.php
+++ src/site/snippets/header.php
@@ -5,10 +5,11 @@
                 <h1 class="logo">
                     <a href="<?php echo $site->url() ?>"><?php echo $site->title() ?></a>
                 </h1>
-                <div class="phone">
-                    <p class="call">Rufen Sie an</p>
-                    <hr>
-                    <p class="tel">0365 - 831 02 54</p>
+                <div class="company-contact">
+                    <p>
+                        <a href="tel:+493658310254" class="tel">Gera — +49 365 831 02 54</a>
+                        <a href="mailto:info@schramm-reinigung.de">info@schramm-reinigung.de</a>
+                    </p>
                 </div>
             </div>
         </div>
