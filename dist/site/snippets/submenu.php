c715a93fc1b03f8c5ece0cff927163733cafc7a4
Booozz <blody.ti@googlemail.com>
2017-08-03 17:35:52 +0200

    update dist to latest

--- dist/site/snippets/submenu.php
+++ dist/site/snippets/submenu.php
@@ -11,11 +11,13 @@ if($root = $pages->findOpen()) {
 
 // only show the menu if items are available
 if($items and $items->count()): ?>
-    <nav class="nav-sub">
-        <ul class="menu-sub">
-            <?php foreach($items as $item): ?>
-                <li><a<?php e($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a></li>
-            <?php endforeach ?>
-        </ul>
-    </nav>
+    <div class="row">
+        <nav class="nav-sub">
+            <ul class="menu-sub">
+                <?php foreach($items as $item): ?>
+                    <li class="col-12 col-sm-6 col-md-4"><a<?php e($item->isOpen(), ' class="active"') ?> href="<?php echo $item->url() ?>"><?php echo $item->title()->html() ?></a></li>
+                <?php endforeach ?>
+            </ul>
+        </nav>
+    </div>
 <?php endif ?>
