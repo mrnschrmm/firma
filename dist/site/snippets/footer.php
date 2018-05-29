4186f72c5bcf4e98c23dbdeed98267bbad5752b5
Marian Schramm <13080099+mrnschrmm@users.noreply.github.com>
2018-05-29 22:18:05 +0200

    remove dist

--- dist/site/snippets/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <p class="copyright"><?php echo $site->copyright() ?></p>
-            </div>
-        </div>
-    </div>
-</footer>
