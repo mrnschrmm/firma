4d90807e9f5569b517e8e00310695afff7de140c
Marian Schramm <13080099+mrnschrmm@users.noreply.github.com>
2019-08-15 21:22:22 +0200

    feat(NavigationMain): add rwd menu behaviour

--- app/snippets/navigation-main/script.js
+++ app/snippets/navigation-main/script.js
@@ -1,15 +1,43 @@
+var $ = window.jQuery
+const { disableBodyScroll, enableBodyScroll } = window.bodyScrollLock
+
 class NavigationMain extends window.HTMLDivElement {
-  constructor (self) {
-    self = super(self)
-    self.resolveElements()
+  constructor (...args) {
+    const self = super(...args)
+    self.init()
     return self
   }
 
+  init () {
+    this.$ = $(this)
+    this.bindFunctions()
+    this.bindEvents()
+    this.resolveElements()
+  }
+
+  bindFunctions () {
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  bindEvents () {
+    this.$.on('click', '.toggle', this.toggleMenu)
+  }
+
   resolveElements () {
+    this.$menu = $('.menu', this)
   }
 
   connectedCallback () {
     console.log('### NAVIGATION-MAIN - SCRIPT.JS ###')
+  }
+
+  toggleMenu (e) {
+    this.$.toggleClass('snippet-component-isOpen')
+    if (this.$.hasClass('snippet-component-isOpen')) {
+      disableBodyScroll(this.$menu.get(0))
+    } else {
+      enableBodyScroll(this.$menu.get(0))
+    }
   }
 }
 
