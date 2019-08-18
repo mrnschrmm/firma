f91e7d86aff4cd86f05deb5d623b64ea65428cd6
Marian Schramm <13080099+mrnschrmm@users.noreply.github.com>
2019-08-18 10:03:57 +0200

    style(Defaults/Spacing): reduce use of breakpoints

--- app/snippets/list-services/index.php
+++ app/snippets/list-services/index.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="list-services">
       <div class="content context-content">
-        <h3 class="title"><?= ucfirst($page->services()->key()) ?></h3>
+        <h2 class="title"><?= ucfirst($page->services()->key()) ?></h2>
         <div class="services">
           <ul class="list">
             <?php foreach ($page->services()->toStructure() as $service): ?>
