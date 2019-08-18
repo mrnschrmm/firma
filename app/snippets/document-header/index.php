61b0b00d5cacc247758061746ae215c645ba17e8
Marian Schramm <13080099+mrnschrmm@users.noreply.github.com>
2019-08-19 00:31:05 +0200

    chore(): update dependencies + minor head item reorder

--- app/snippets/document-header/index.php
+++ app/snippets/document-header/index.php
@@ -2,19 +2,19 @@
 <html class="html <?= $page->template() ?> theme-default" lang="<?= $kirby->language()->code() ?>">
 
 <head>
-  <title><?= $site->title() ?></title>
   <meta charset="utf-8">
+  <title><?= $site->title() ?></title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="<?= $site->description() ?>">
-  <meta name="author" content="<?= $site->author() ?>">
   <meta name="language" content="<?= $kirby->language()->code() ?>">
-  <meta name="robots" content="index, follow, noodp">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="<?= $site->author() ?>">
+  <meta name="robots" content="noodp">
+  <link rel="stylesheet" href="<?= url('main.min.css') ?>">
+  <link rel="shortcut icon" sizes="16x16 32x32" href="<?= url('/assets/favicons/favicon.ico') ?>">
   <link rel="icon" type="image/png" sizes="16x16" href="<?= url('/assets/favicons/favicon-16x16.png') ?>">
   <link rel="icon" type="image/png" sizes="32x32" href="<?= url('/assets/favicons/favicon-32x32.png') ?>">
-  <link rel="shortcut icon" sizes="16x16 32x32" href="<?= url('/assets/favicons/favicon.ico') ?>">
   <link rel="canonical" href="<?= $page->url($kirby->language()->code()) ?>">
-  <link rel="stylesheet" href="<?= url('main.min.css') ?>">
   <script src='<?= url('vendor.head.min.js') ?>'></script>
 </head>
 
