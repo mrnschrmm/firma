bca4c6aaf650ba0d014ac5eb96f3f3333496199e
mrnschrmm <>
2019-11-26 11:53:13 +0100

    update(): mail card-contact

--- app/snippets/component/card-contact/index.php
+++ app/snippets/component/card-contact/index.php
@@ -3,7 +3,7 @@
   <div class="card-contact">
     <div class="details">
       <a class="details-tel" href="tel:+493658310254" rel="nofollow">Gera — +49 365 831 02 54</a>
-      <a class="details-mail" href="mailto:info@schramm-reinigung.de" rel="nofollow">info@schramm-reinigung.de</a>
+      <a class="details-mail" href="mailto:hello@schramm-reinigung.de" rel="nofollow">hello@schramm-reinigung.de</a>
     </div>
   </div>
 </div>
