025bde8d89d38eee2f66bdd36add77f9e4cd249f
mrnschrmm <>
2020-01-01 06:37:29 +0100

    feat(): extend panel/blueprints

--- app/snippets/document/header/index.php
+++ app/snippets/document/header/index.php
@@ -2,13 +2,14 @@
 <html class="app <?= $page->template() ?> theme-default" lang="<?= $kirby->language()->code() ?>">
 
   <head>
+    <title><?= $site->title()->html() ?></title>
     <meta charset="utf-8">
-    <title><?= $site->title() ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="<?= $site->description() ?>">
+    <meta name="description" content="<?= $site->description()->html() ?>">
+    <meta name="keywords" content="<?= $site->keywords()->html() ?>">
+    <meta name="author" content="<?= $site->author()->html() ?>">
     <meta name="language" content="<?= $kirby->language()->code() ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="<?= $site->author() ?>">
     <meta name="robots" content="noodp">
     <link rel="stylesheet" href="<?= url('main.min.css') ?>">
     <link rel="shortcut icon" sizes="16x16 32x32" href="<?= url('/assets/favicons/favicon.ico') ?>">
