37c3f64b38c73d263e10cef22e00363921988dd1
mrnschrmm <>
2020-01-04 01:55:39 +0100

    feat(): toggle notify

--- app/snippets/document/side/index.php
+++ app/snippets/document/side/index.php
@@ -1,3 +1,1 @@
-<div class="app-side">
-  <?php snippet('document/side/partials/block-notify/index') ?>
-</div>
+<div class="app-side"><?php if ($site->privacy_user_statistics()->isTrue()): ?><?php snippet('document/side/partials/block-notify/index') ?><?php endif ?></div>
