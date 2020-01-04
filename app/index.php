53e1cc69ba600bdd2ba498dfda9493a0844865d6
mrnschrmm <>
2020-01-04 02:35:09 +0100

    update(): index.php

--- app/index.php
+++ app/index.php
@@ -20,3 +20,26 @@ $kirby = new Kirby([
 ]);
 
 echo $kirby->render();
+
+// <?php
+// include '../kirby/bootstrap.php';
+
+// $kirby = new Kirby([
+//     'urls' => [
+//         'index' => 'https://www.schramm-reinigung.de',
+//         'media' => 'https://www.schramm-reinigung.de/media',
+//     ],
+//     'roots' => [
+//         'index'    => __DIR__,
+//         'base'     => $base    = dirname(__DIR__),
+//         'content'  => $base . '/content',
+//         'site'     => $base . '/site',
+//         'kirby'    => $base . '/kirby',
+//         'storage'  => $storage = $base . '/storage',
+//         'accounts' => $storage . '/accounts',
+//         'cache'    => $storage . '/cache',
+//         'sessions' => $storage . '/sessions',
+//     ]
+// ]);
+
+// echo $kirby->render();
