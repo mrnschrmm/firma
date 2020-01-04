6d0a87f921e5bbc2ca97b8cc4a3a78c0ffdfafa1
mrnschrmm <>
2020-01-04 02:49:56 +0100

    update(): index.php

--- app/index.php
+++ app/index.php
@@ -1,10 +1,30 @@
 <?php
-include '../vendor/autoload.php';
+// include '../vendor/autoload.php';
+
+// $kirby = new Kirby([
+//     'urls' => [
+//         'index' => 'http://firma.local.run:8080',
+//         'media' => 'http://firma.local.run:8080/media',
+//     ],
+//     'roots' => [
+//         'index'    => __DIR__,
+//         'base'     => $base    = dirname(__DIR__),
+//         'content'  => $base . '/content',
+//         'site'     => $base . '/site',
+//         'kirby'    => $base . '/kirby',
+//         'storage'  => $storage = $base . '/storage',
+//         'accounts' => $storage . '/accounts',
+//         'cache'    => $storage . '/cache',
+//         'sessions' => $storage . '/sessions',
+//     ]
+// ]);
+
+include '../kirby/bootstrap.php';
 
 $kirby = new Kirby([
     'urls' => [
-        'index' => 'http://firma.local.run:8080',
-        'media' => 'http://firma.local.run:8080/media',
+        'index' => 'https://www.schramm-reinigung.de',
+        'media' => 'https://www.schramm-reinigung.de/media',
     ],
     'roots' => [
         'index'    => __DIR__,
@@ -20,26 +40,3 @@ $kirby = new Kirby([
 ]);
 
 echo $kirby->render();
-
-// <?php
-// include '../kirby/bootstrap.php';
-
-// $kirby = new Kirby([
-//     'urls' => [
-//         'index' => 'https://www.schramm-reinigung.de',
-//         'media' => 'https://www.schramm-reinigung.de/media',
-//     ],
-//     'roots' => [
-//         'index'    => __DIR__,
-//         'base'     => $base    = dirname(__DIR__),
-//         'content'  => $base . '/content',
-//         'site'     => $base . '/site',
-//         'kirby'    => $base . '/kirby',
-//         'storage'  => $storage = $base . '/storage',
-//         'accounts' => $storage . '/accounts',
-//         'cache'    => $storage . '/cache',
-//         'sessions' => $storage . '/sessions',
-//     ]
-// ]);
-
-// echo $kirby->render();
