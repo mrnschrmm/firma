2de0c18b11eab51bdc84c5de75c8c86f0fa6b9fc
mrnschrmm <>
2020-01-07 00:16:48 +0100

    update(): line endings

--- app/snippets/document/header/index.php
+++ app/snippets/document/header/index.php
@@ -6,13 +6,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php if ($site->site_description()->isNotEmpty()): ?>
+    <?php if ($site->site_description()->isNotEmpty()) : ?>
     <meta name="description" content="<?= $site->site_description()->h() ?>">
     <?php endif ?>
-    <?php if ($site->site_keywords()->isNotEmpty()): ?>
+    <?php if ($site->site_keywords()->isNotEmpty()) : ?>
     <meta name="keywords" content="<?= $site->site_keywords()->h() ?>">
     <?php endif ?>
-    <?php if ($site->site_author()->isNotEmpty()): ?>
+    <?php if ($site->site_author()->isNotEmpty()) : ?>
     <meta name="author" content="<?= $site->site_author()->h() ?>">
     <?php endif ?>
     <meta name="language" content="<?= $kirby->language()->code() ?>">
@@ -23,8 +23,8 @@
     <link rel="icon shortcut" sizes="16x16 32x32" href="<?= url('/assets/favicons/favicon.ico') ?>">
     <link rel="stylesheet" href="<?= url('main.min.css') ?>">
     <script src='<?= url('vendor.head.min.js') ?>'></script>
-    <?php if ($site->privacy_metrics()->isTrue()): ?>
-    <?php snippet('matomo'); ?>
+    <?php if ($site->privacy_metrics()->isTrue()) : ?>
+    <?php snippet('matomo') ?>
     <?php endif ?>
   </head>
 
