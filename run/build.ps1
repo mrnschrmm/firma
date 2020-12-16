acb2b3224e9d45780c48d2e8ea1e1020f2efaf40
mrnmrhcs <m.service@kaimberg.com>
2020-12-16 10:20:09 +0100

    update(): start + build run script

--- run/build.ps1
+++ run/build.ps1
@@ -1,62 +1,18 @@
-# BUILD
-try {
-    Write-Host '## RUN ## BUILD'
-    Write-Host
+Write-Host '## RUN ## BUILD'
+Write-Host
 
-    if (Test-Path '.\app\composer.json') {
-        Write-Host 'WORKING...'
-        Copy-Item '.\app\composer.json' -Destination '.\dist'
-        composer update -d '.\dist'
-    }
+Import-Module '.\run\module\env.psm1'
+Update-Composer -Mode 'Init' -ErrorAction Stop
 
-    Write-Host 'LINT'
+Write-Host 'LINT'
 
-    $lint = @(
-        { .\dist\vendor\bin\phpcs --standard='.\phpcs.ruleset.xml' --report=summary '.\app' -v },
-        { npx stylelint './app/snippets/**/*.scss' './app/resources/**/*.scss' },
-        { npx eslint './app/resources/main.js' './app/resources/panel.js' './app/snippets/**/script.js' }
-    )
+.\dist\vendor\bin\phpcs --standard='.\phpcs.ruleset.xml' --report=summary '.\app' -v
+npx stylelint './app/snippets/**/*.scss' './app/resources/**/*.scss'
+npx eslint './app/resources/main.js' './app/resources/panel.js' './app/snippets/**/script.js'
 
-    foreach ($item in $lint) {
-        try {
-            Write-Host 'WORKING...'
-            & $item
-        } catch {
-            Write-Host 'ERROR'
-            $_.Exception.Message
-            exit 1
-        }
-    }
+Write-Host 'BUILD'
 
-    Write-Host 'BUILD'
+gulp
 
-    $build = @(
-        { gulp },
-        { composer install -d '.\dist' --no-dev }
-    )
-
-    foreach ($item in $build) {
-        try {
-            Write-Host 'WORKING...'
-            & $item
-
-        } catch {
-            Write-Host 'ERROR'
-            $_.Exception.Message
-            exit 1
-        }
-    }
-
-    if (Test-Path '.\dist\composer.*') {
-        Remove-Item '.\dist\composer.*'
-        Write-Host 'CLEANUP...'
-    }
-
-    Write-Host 'COMPLETE'
-    exit 0
-
-} catch {
-    Write-Host 'ERROR'
-    $_.Exception.Message
-    exit 1
-}
+Update-Composer -Mode 'Clear'
+Write-Host 'COMPLETE'
--- run/start.ps1
+++ run/start.ps1
@@ -1,27 +1,7 @@
-# START
-try {
-    Write-Host '## RUN ## START'
-    Write-Host
-    Write-Host 'WORKING...'
+Write-Host '## RUN ## START'
+Write-Host
 
-    $run = @(
-        { composer update -d '.\' --root-reqs },
-        { gulp }
-    )
+Import-Module '.\run\module\env.psm1'
+Update-Composer -Mode 'Init'
 
-    foreach ($item in $run) {
-        try {
-            & $item
-        } catch {
-            Write-Host 'ERROR'
-            $_.Exception.Message
-            exit 1
-        }
-    }
-    exit 0
-
-} catch {
-    Write-Host 'ERROR'
-    $_.Exception.Message
-    exit 1
-}
+gulp
